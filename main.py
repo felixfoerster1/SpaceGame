8dea97206cdb816eb62a68cdc07921eb8685ca36
Maxirepa <Max18lja@gmail.com>
2022-05-23 08:07:37 +0200

    game weitergemacht

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ import pgzrun
 import random
 
 HEIGHT = 800
-WITH = 800
+WIDTH = 800
 
-
+score=0
 ship = Actor("playership1_blue.png")
 ship.x = 50
-ship.y = 100
+ship.y = 700
 
 gem = Actor("gemgreen.png")
 gem.x = random.randint(0 , 800)
 gem.y = 0
-
+def draw():
+    screen.clear()
+    global score
+    gem.draw()
+    ship.draw()
+    screen.draw.text(f'Score: {score}',(0,0), fontsize=40, color=(255,255,255))
 
 def update():
     global score
     gem.y+= 4
-    if keyboard.right():
+    if keyboard.right:
         ship.x +=5
-    if keyboard.left():
+    if keyboard.left:
         ship.x +=-5
     if ship.colliderect(gem):
         score+=10
+        gem.y=0
+        gem.x= random.randint(0,800)
     if gem.y>= 750:
         gem.y=0
         gem.x= random.randint(0,800)
