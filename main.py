89e09d8e9f212f9483cccdfdaf3a8637f6b52ce9
LeoBiewer <leo@wohlnick.de>
2022-05-23 08:31:07 +0200

    für fortneit

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def draw():
     screen.clear()
     global score
     gem1.draw()
+    gem2.draw()
+    gem3.draw()
     ship.draw()
     screen.draw.text(f'Score: {score}',(0,0), fontsize=40, color=(255,255,255))
 
@@ -38,7 +40,7 @@ def update():
     if keyboard.left:
         ship.x +=-5
     if ship.colliderect(gem1):
-        score+=10
+        score+=20
         gem1.y=0
         gem1.x= random.randint(0,800)
     if gem1.y>= 750:
@@ -46,4 +48,21 @@ def update():
         gem1.x= random.randint(0,800)
         score+= -20
 
+    if ship.colliderect(gem2):
+        score+=10
+        gem2.y=0
+        gem2.x= random.randint(0,800)
+    if gem2.y>= 750:
+        gem2.y=0
+        gem2.x= random.randint(0,800)
+        score+= -50
+
+    if ship.colliderect(gem3):
+        score+=50
+        gem3.y=0
+        gem3.x= random.randint(0,800)
+    if gem3.y>= 750:
+        gem3.y=0
+        gem3.x= random.randint(0,800)
+        score+= -10
 pgzrun.go()
