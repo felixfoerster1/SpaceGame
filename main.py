2d2c817cba34991be79c13ac43ef8df28b808b2f
LeoBiewer <97019099+LeoBiewer@users.noreply.github.com>
2022-05-23 08:26:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 import pgzrun
 import random
-import os
 
 HEIGHT = 800
 WIDTH = 800
-
-
-for image in images:
-    if image.startswith('gem'):
-        images.remove(image)
 
 score=0
 ship = Actor("playership1_blue.png")
 ship.x = 50
 ship.y = 700
 
-gem = Actor("gemgreen.png")
-gem.x = random.randint(0 , 800)
-gem.y = 0
+gem1 = Actor("gemgreen.png")
+gem1.x = random.randint(0 , 800)
+gem1.y = 0
+
+gem2 = Actor("gemred.png")
+gem2.x = random.randint(0 , 800)
+gem2.y = 0
+
+gem3 = Actor("gemyellow.png")
+gem3.x = random.randint(0 , 800)
+gem3.y = 0  
+
 def draw():
     screen.clear()
     global score
-    gem.draw()
+    gem1.draw()
     ship.draw()
     screen.draw.text(f'Score: {score}',(0,0), fontsize=40, color=(255,255,255))
 
 def update():
     global score
-    gem.y+= 4
+    gem1.y+= 4
+    gem2.y+= 1
+    gem3.y+= 6
     if keyboard.right:
         ship.x +=5
     if keyboard.left:
         ship.x +=-5
-    if ship.colliderect(gem):
-        r = random.randint(0, len(images) - 1)
-        ship.image = images[r]
+    if ship.colliderect(gem1):
         score+=10
-        gem.y=0
-        gem.x= random.randint(0,800)
-    if gem.y>= 750:
-        gem.y=0
-        gem.x= random.randint(0,800)
+        gem1.y=0
+        gem1.x= random.randint(0,800)
+    if gem1.y>= 750:
+        gem1.y=0
+        gem1.x= random.randint(0,800)
         score+= -20
-    
 
 pgzrun.go()
